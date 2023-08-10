d402d1a966bbcf6738c168932d74cdc2fde83e80
younis <younistaher12341@gmail.com>
2023-08-10 11:10:34 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
-
 /**
  * main - Entry point
  *
@@ -9,15 +8,15 @@
  */
 int main(void)
 {
+
 	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	char buf2[];
 
-	  Fd[0] = open(“sample.txt”, O_CREATE);
-	  Fd[1] = open(“sample.txt”, O_CREATE);
+	  int Fd = open(“sample.txt”, O_CREATE);
+	  /*Fd[1] = open(“sample.txt”, O_CREATE);*/
 
-	  Write(fd[0], buf1, strlen(buf1));
-	  Write(1, buf2, read(fd[1], buf2, 12));
+	  Write(fd, buf1, strlen(buf1));
+	  /*Write(1, buf2, read(fd[1], buf2, 12));*/
 
-	  Close(fd[0]);
-	  Close(fd[1]);
+	  Close(fd);
 }
