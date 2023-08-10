a5b0a39ec4989ab8a8d3ca822df60f4e5f05be0b
younis <younistaher12341@gmail.com>
2023-08-10 11:28:03 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,6 @@ int main(void)
 
 	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	  /*Fd[1] = open(“sample.txt”, O_CREATE);*/
-
-	  write(1, buf1, strlen(buf1));
-	  /*Write(1, buf2, read(fd[1], buf2i, 12));*/
+	write(1, buf1, strlen(buf1));
+	return 1;
 }
