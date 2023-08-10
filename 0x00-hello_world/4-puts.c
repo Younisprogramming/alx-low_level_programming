81683f90ac47f8d29d5b6d9890f7e8a7ea601644
younis <younistaher12341@gmail.com>
2023-08-10 07:37:56 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-/*
- * main-entry point
- * Return: Always (success)
- */
-/*main*/
+#include<stdio.h>
+/*main function*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
