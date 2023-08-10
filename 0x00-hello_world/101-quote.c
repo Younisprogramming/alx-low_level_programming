32aa4d658b5b64e1d73a6e1a6f95e62785bf5577
younis <younistaher12341@gmail.com>
2023-08-10 11:29:41 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 
-	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char buf1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, buf1, strlen(buf1));
 	return 1;
