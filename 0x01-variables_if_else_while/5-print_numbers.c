edc0d91b3a37f7fd82c9ff47decbf6f18c96c00a
younis <younistaher12341@gmail.com>
2023-08-11 07:40:43 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int x = 0 ; x < 10 ; x++)
+	{
+		printf(x);
+	}
+	printf("\n");
+	return (0);
+}
+
