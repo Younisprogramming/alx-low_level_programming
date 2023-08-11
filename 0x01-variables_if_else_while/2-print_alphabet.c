3809dc471bf4837437d075aa92aeaf7128112f62
younis <younistaher12341@gmail.com>
2023-08-11 06:43:46 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succsess)
+ */
+int main(void)
+{
+	for (int x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
