3fdb2fdab228187e94c1e2cec9075e5737e5a2f3
younis <younistaher12341@gmail.com>
2023-08-11 11:10:51 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+	int i = 48;
+
+	while (x < 58)
+	{
+		while (i < 58)
+		{
+			if (x != i && x < i)
+			{
+				putchar(x);
+				putchar(i);
+				if (i == 57 && x == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		   i++;
+		}
+	    x++;
+	}
+	putchar('\n');
+	return (0);
+}
