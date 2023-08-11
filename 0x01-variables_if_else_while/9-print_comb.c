c2cd6ab9383e8f41e258d382d8f01ebe51c1201c
younis <younistaher12341@gmail.com>
2023-08-11 09:29:45 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@ int main(void)
 {
 	int x = 0;
 
-	while (x < 9)
+	while (x < 10)
 	{
 		putchar('0' + x);
-		putchar(',');
-		putchar(' ');
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		x++;
 	}
-	putchar('9');
+	putchar(' ');
 	return (0);
 }
