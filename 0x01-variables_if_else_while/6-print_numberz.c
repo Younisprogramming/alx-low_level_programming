013573e655a2c0195198016f34b60b3b905662c9
younis <younistaher12341@gmail.com>
2023-08-11 08:19:35 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int x = 0 ; x < 10 ; x++)
+	{
+		putchar('0' + x);
+	}
+	putcahr('\n');
+	return (0);
+}
