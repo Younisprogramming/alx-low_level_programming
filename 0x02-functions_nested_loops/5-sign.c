35d05c1f03a8fa81c90baebb7e31965468ccfb5b
younis <younistaher12341@gmail.com>
2023-08-15 10:55:42 -0700

    c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - prints + 0 -
+ *
+ *@n: an integer parameter
+ *
+ * Return: + for lowercase number. 0 for zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
