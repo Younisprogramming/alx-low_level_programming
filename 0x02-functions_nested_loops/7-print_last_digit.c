bfd39fb5313dc1e2751059a0efbcbc5e65648df3
younis <younistaher12341@gmail.com>
2023-08-15 11:23:19 -0700

    c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_last_digit - Shows 1 if the input is a
+ *
+ *@i: The character in ASCII code
+ *
+ *Return:
+ */
+int print_last_digit(int i)
+{
+	int x = i % 10;
+
+	return (x);
+}
+
+
