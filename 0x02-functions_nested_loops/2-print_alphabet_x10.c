341afb1e3f4745a2a96084875d5aa713719a48d1
younis <younistaher12341@gmail.com>
2023-08-15 08:39:53 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	int x = 'a';
+
+	while (i < 10)
+	{
+		x = 'a';
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
+
