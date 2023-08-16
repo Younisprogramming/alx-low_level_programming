9e2aae0dd5c48da18ca467d27e5123284fa0cef7
younis <younistaher12341@gmail.com>
2023-08-15 17:30:16 -0700

    c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * jack_bauer - wdwdwdw
+ *
+ * Return: fawfwf
+ */
+void jack_bauer(void)
+{
+	int a = 48;
+	int b;
+	int c;
+	int d;
+
+	while (a < 51)
+	{
+		b = 48;
+		while (b < 58)
+		{
+			c = 48;
+			while (c < 48)
+			{
+				d = 48;
+				while (d < 48)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(':');
+					putchar(c);
+					putchar(d);
+					d++;
+					putchar('\n);
+				}
+				c++;
+			}
+			if (b == 51)
+			{
+				break;
+			}
+			b++:
+		}
+		a++;
+	}
+}
