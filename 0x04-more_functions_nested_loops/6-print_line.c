2bbe9dbb4a120a766c1c7382b2ca294c7c9690bc
younis <younistaher12341@gmail.com>
2023-08-17 05:50:58 -0700

    c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,18 @@
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-	for (int i = 0 ; i < n ; i++)
-	{
-		_putchar('_');
-	}
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar('_');
+		}
 	_putchar('\n');
 	}
 }
