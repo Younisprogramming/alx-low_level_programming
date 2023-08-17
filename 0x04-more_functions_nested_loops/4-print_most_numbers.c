006cfb715e20639bdbd1a17c095b96161cd62035
younis <younistaher12341@gmail.com>
2023-08-17 04:49:56 -0700

    c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_most_numbers - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int s = 0;
+
+	while (s < 10)
+	{
+		if (s == 2 || s == 4)
+			s++;
+		_putchar('0' + s);
+	}
+	_putchar('\n');
+}
