ab13ff5b87e9088ceeabab08f08734375859a2a3
younis <younistaher12341@gmail.com>
2023-08-17 04:34:25 -0700

    c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include "main.h"
+/**
+ * print_numbers - awdwa
+ *
+ * Return: No
+ */
+void print_numbers(void)
+{
+	int s = 0;
+
+	while (s < 10)
+	{
+		_putchar('0' + s);
+		s++;
+	}
+	_putchar('\n');
+}
