e6766fd9a34ffc3c486679397689c4a8e54e8efd
younis <younistaher12341@gmail.com>
2023-08-17 06:39:52 -0700

    c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *  * print_square - check the code
+ *
+ *@size: awwddw
+ *
+ *    * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i;
+	int s;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (s = 0 ; s < n ; s++)
+			{
+				putchar(35);
+			}
+			putchar('\n');
+		}
+	}
+
+}
