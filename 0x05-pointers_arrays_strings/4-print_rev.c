ee6ea59fc6157cf7cd781727e4abc6999f97ceb5
younis <younistaher12341@gmail.com>
2023-08-21 13:13:55 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *  *print_rev- awdwdw
+ *   *
+ *    *@s: 1
+ *      *
+ *       *Return: No
+ */
+void print_rev(char *s)
+{
+	int i = strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
