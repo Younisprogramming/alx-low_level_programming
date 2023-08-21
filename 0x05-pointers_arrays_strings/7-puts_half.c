b6182214105ade26f1b8e96609e4f057d808b214
younis <younistaher12341@gmail.com>
2023-08-21 14:23:13 -0700

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "string.h"
+#include "stdio.h"
+/**
+ *  *puts_half - awdwdw
+ *   *
+ *    *@str: 1
+ *      *
+ *       *Return: No
+ */
+void puts_half(char *str)
+{
+	int n;
+	int i = strlen(str);
+
+	if ((strlen(str) % 2) == 0)
+	{
+		n = strlen(str) / 2;
+	}
+	else
+	{
+		n = (strlen(str) - 1) / 2;
+	}
+	while (n < i)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
