b8bf1ab3d6e69353988ef2aa58d223a9965e3a2c
younis <younistaher12341@gmail.com>
2023-08-21 14:04:38 -0700

    c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "string.h"
+#include "stdio.h"
+/**
+ *  *puts2- awdwdw
+ *   *
+ *    *@str: 1
+ *      *
+ *       *Return: No
+ */
+void puts2(char *str)
+{
+	int a = 0;
+	int b = strlen(str)
+
+	while (a < b)
+	{
+		_putchar(str[a]);
+		a = a + 2;
+	}
+	_putchar('\n');
+}
