3a578a9a180ed3d4f46d2b42d5cb3efe84cefca9
younis <younistaher12341@gmail.com>
2023-08-24 13:31:12 -0700

    c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *  *_strncpy - concatenates two strings.
+ *   *
+ *    *@dest:awdwad
+ *     *@src:dwdawd
+ *      *@n:dawdwa
+ *
+ *       * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (count < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
