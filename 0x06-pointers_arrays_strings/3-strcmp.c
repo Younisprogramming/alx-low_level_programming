94039b2169d4d46e8629dc45eb394dec658cebc1
younis <younistaher12341@gmail.com>
2023-08-24 14:29:16 -0700

    c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,23 @@
 int _strcmp(char *s1, char *s2)
 {
 	int count = 0;
+	int s = strlen(s1);
 
-	if (s1 == s2)
-	return (0);
-	else
+	while (s1[count] != '\0' || s2[count] != '\0')
 	{
+		if (s1[count] == s2[count])
+			s--;
+		if (s == 0)
+			return (0);
 		count++;
-		while (count < strlen(s1) && s1[count] != '\0')
-		{
-			if (s1[count] != s2[count])
-			{
-				return (s1[count] - s2[count]);
-			}
-			count++;
-		}
+	}
+
+	count = 0;
+
+	while (count < strlen(s1) && s1[count] != '\0')
+	{
+		if (s1[count] != s2[count])
+			return (s1[count] - s2[count]);
+		count++;
 	}
 }
