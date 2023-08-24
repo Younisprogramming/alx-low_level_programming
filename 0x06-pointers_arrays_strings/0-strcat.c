15e6ab47a907c46a1ec8f22a00496c594e50b898
younis <younistaher12341@gmail.com>
2023-08-24 12:09:43 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcat - concatenates two strings.
+ *
+ *@dest:awdwad
+ *@src:dwdawd
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	char pt[strlen(dest)];
+	int i;
+	int s;
+
+	for (i = 0 ; i < strlen(dest) ; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	s = strlen(s1);
+
+	for (i = 0 ; i < strlen(src) ; i++)
+	{
+		ptr[s] = s2[i];
+		s++;
+	}
+	return (pt);
+}
