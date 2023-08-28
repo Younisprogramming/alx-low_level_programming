1ac95fe7f200beea845e8a654c6d65c4440452b9
younis <younistaher12341@gmail.com>
2023-08-28 09:10:25 -0700

    c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  *_memcpy - fills memory with a constant byte
+ *   *
+ *    * @dest:vvv
+ *     * @src:vvv
+ *      * @n:vvv
+ *       *
+ *        * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+}
