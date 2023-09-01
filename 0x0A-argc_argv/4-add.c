ba696deff31df7b7ad7fa4193ff1a57cd58d9df0
younis <younistaher12341@gmail.com>
2023-09-01 14:04:55 -0700

    c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int i;
+	long unsigned int j;
+
+	for (i = 1 ; i < argc ; i++)
+	{
+		for (j = 0 ; j < strlen(argv[i]) ; j++)
+		{
+			if (!(argv[i][j] >= 48 && argv[i][j] <= 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		count += atoi(argv[i]);
+	}
+	printf("%d\n", count);
+	return (0);
+}
