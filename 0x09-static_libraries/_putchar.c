587cca3010b6500f211a16d4f233bc417a6cd854
younis <younistaher12341@gmail.com>
2023-09-01 09:30:26 -0700

    c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,21 +1,13 @@
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-
-	int i = 0;
-
-	while (i < 8)
-	{
-		putchar(str[i]);
-		i++;
-	}
-	putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
