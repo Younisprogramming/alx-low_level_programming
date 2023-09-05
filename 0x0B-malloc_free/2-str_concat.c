3b9739941be52a5257cfe692d8c8335cc0b8b737
younis <younistaher12341@gmail.com>
2023-09-05 07:53:02 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	unsigned int a, j, i = strlen(s1) + strlen(s2);
+	unsigned int a, j, i;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
-		s1 = "";
+		s2 = "";
+	i = strlen(s1) + strlen(s2);
 
 	p = malloc(sizeof(char) * i + 1);
+
+	if (p == NULL)
+		return (NULL);
 
 	for (j = 0 ; j < strlen(s1) ; j++)
 	{
