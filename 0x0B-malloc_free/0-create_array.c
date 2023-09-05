26f67787b1367dc76f5b12760115d242638351f5
younis <younistaher12341@gmail.com>
2023-09-05 06:33:06 -0700

    c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,13 @@ char *create_array(unsigned int size, char c)
     unsigned int i;
     
     if (size == 0)
-    {
         return (NULL);
-    }
     
     p = malloc(sizeof(c) * size);
     
+    if (P == NULL)
+	    return (NULL);
+
     for(i = 0 ; i < size ; i++)
     {
         p[i] = c;
