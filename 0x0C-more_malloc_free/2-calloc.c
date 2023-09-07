a1206e5f56b3bc8712e465257b92279d077f8127
younis <younistaher12341@gmail.com>
2023-09-07 12:08:06 -0700

    c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = malloc(size * nmemb);
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (p == NULL)
 		return (NULL);
+	for (i = 0 ; i < nmemb ; i++)
+		p[i] = 0;
 	return (p);
 }
