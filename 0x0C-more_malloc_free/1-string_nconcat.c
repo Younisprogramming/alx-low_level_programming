1b001060ab4252a12a9aaef80cb5167e5ab96620
younis <younistaher12341@gmail.com>
2023-09-07 10:14:28 -0700

    c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned long int i;
 	char *p;
 
-	if (n >= strlen(s2))
+	if (n > strlen(s2))
 		n = strlen(s2);
 	if (s1 == NULL)
 		p = malloc(n + 1);
