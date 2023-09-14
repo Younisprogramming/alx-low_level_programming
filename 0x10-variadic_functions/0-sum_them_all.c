074cd907ad1675a67aa6a89a59090a377863eb26
younis <younistaher12341@gmail.com>
2023-09-14 16:18:13 -0700

    number 0 done yeaaaaah

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,12 +7,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	if (n == 0)
+		return (0);
 	va_list ap;
-	int i , sum =0;
+	int i, sum = 0;
 
 	va_start(ap, n);
 	for (i = n ; i == 0 ; i = va_arg(ap, int))
 		sum += i;
 	va_end(ap);
 	return (sum);
+
 }
