ec8b1117a95c13238039cf131ce5487ddc00af39
Younisprogramming <younistaher12341@gmail.com>
2023-09-25 11:24:05 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,11 +11,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (head == NULL)
 	{
-		*head = ptr;
-		return (ptr);
+		return (NULL);
 	}
 	ptr->n = n;
 	ptr->next = *head;
 	*head = ptr;
-	return (ptr);
+	return (*head);
 }
