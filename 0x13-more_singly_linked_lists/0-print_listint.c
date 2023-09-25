200076384797cef1a39a83517e2be360acd3d6b2
Younisprogramming <younistaher12341@gmail.com>
2023-09-25 09:53:04 -0700

    let's gooo

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+/**
+ * print_listint - dawdwad
+ * @h: dawd
+ * Return: count of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
