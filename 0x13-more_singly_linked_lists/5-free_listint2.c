dae5a7c7f44bd0fdd8fa8a4b3a4eaf7a77ed4063
Younisprogramming <younistaher12341@gmail.com>
2023-09-25 14:09:00 -0700

    test 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 	listint_t *temp, *ptr;
 
 	ptr = *head;
-	while (head != NULL)
+	while (ptr != NULL)
 	{
 		temp = ptr;
 		ptr = ptr->next;
 		free(temp);
 	}
-	head = NULL;
+	*head = NULL;
 }
