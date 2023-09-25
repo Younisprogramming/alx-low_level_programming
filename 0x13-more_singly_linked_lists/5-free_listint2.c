ca3e10fa62c4cd23a78139260c3f1cc098f57fa6
Younisprogramming <younistaher12341@gmail.com>
2023-09-25 13:56:57 -0700

    test 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - abc
+ * @head: aa
+ * Return: we say its void !!
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *ptr;
+
+	ptr = *head;
+	while (head != NULL)
+	{
+		temp = ptr;
+		ptr = ptr->next;
+		free(temp);
+	}
+	head = NULL;
+}
