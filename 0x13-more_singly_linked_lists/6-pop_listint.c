bb6f2bc4063b241864f313830bdfe1d00f5983ab
Younisprogramming <younistaher12341@gmail.com>
2023-09-25 15:19:37 -0700

    test

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,10 +7,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n = 0;
 
 	if (*head == NULL)
 		return (0);
 	temp = *head;
+	n = temp->n;
 	temp = temp->next;
 	free(*head);
 	*head = NULL;
