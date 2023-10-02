3862a5ca777936c60a67bd66ff153b58c8d808f3
Younisprogramming <younistaher12341@gmail.com>
2023-10-02 13:01:22 -0700

    first

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - aaa
+ * @filename: name of the file
+ * @letters: number of char...
+ * Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char c;
+	unsigned int i = 0;
+
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return (0);
+	while (i < letters)
+	{
+		c = fgetc(fp);
+		if (feof(fp))
+			break;
+		_putchar(c);
+		i++;
+	}
+	fclose(fp);
+	return (i);
+}
