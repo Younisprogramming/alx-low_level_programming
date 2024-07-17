8cb32d7143dcd7bc69339b60fde9f57d9ac51738
younisprogramming <younistaher12341@gnail.com>
2024-07-17 15:57:57 -0700

    task0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,23 +2,25 @@
 #include "search_algos.h"
 /**
  * linear_search - linear search algos
- * @array: a pinter t array 
+ * @array: a pinter t array
  * @size: the size of array
- * @value: the value we will search for ... 
+ * @value: the value we will search for ...
  * Return: the index of the value or -1
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if(array == NULL)
-        return (-1);
+	size_t i = 0;
 
-    for(int i = 0; i < size ; i++)
-    {
-        printf("Value checked array[%d] = %d\n", i, *array);
-        if (*array == value)
-            return (i);
-        else
-            array++;
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = %d\n", i, *array);
+		if (*array == value)
+			return (i);
+		array++;
+		i++;
+	}
+	return (-1);
 }
